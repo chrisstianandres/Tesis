2b861af6eb106ea8800cae98611f6ab514501cd3
Chrisstianandres <Chrisstianandres@gmail.com>
2020-04-23 18:11:53 -0500

    proyecto

---
+++ apps/asistencias/views.py
@@ -0,0 +1,133 @@
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+import pandas as pd
+import numpy as np
+from .models import *
+from apps.horario.models import *
+from apps.asistencias.forms import *
+from apps.asistencias.models import Asistencias
+import json
+from django.http import *
+
+def alumno_list(request):
+    a = request.user.id
+    materia = request.GET.get('materia')
+    nomCurso = request.GET.get('nomCurso')
+    listaalumno = Asistencias.objects.raw('SELECT max(horario."id") AS id,curso.nombre , '
+                                          'lista_listado."id" as idlista, alumno.nombres,'
+                                          'alumno.apellidos,current_date as hoy, materia.nombre as materia  '
+                                          'FROM horario INNER JOIN curso ON horario.curso_id = curso."id" '
+                                          'INNER JOIN lista_listado ON lista_listado.curso_id = curso."id" '
+                                          'INNER JOIN alumno ON lista_listado.alumno_id = alumno."id" '
+                                          'INNER JOIN silabo ON "public".horario.silabo_id= silabo."id" '
+                                          'INNER JOIN materia ON silabo.materia_id = materia."id"  '
+                                          'WHERE horario.docente_id= %s and curso.nombre= %s  '
+                                          'and materia.nombre = %s GROUP BY curso.nombre , lista_listado."id",'
+                                          'alumno.nombres,alumno.apellidos,materia.nombre '
+                                          'order by alumno.apellidos asc ', [a, nomCurso, materia])
+    contexto = {'listaalumno': listaalumno}
+
+    return render(request, "back-end/alumno/alumnoindex.html", contexto)
+def alumno_list2(request):
+    p= 'Presente'
+    f='Falta'
+    a = request.user.id
+    materia = request.GET.get('materia')
+    fecha = request.GET.get('fecha')
+    nomCurso = request.GET.get('nomCurso')
+    listaalumno = Asistencias.objects.raw('SELECT alumno.nombres, alumno.apellidos, '
+                                          'asistencias_asistencias."Asistencia" '
+                                          'AS "id",Case asistencias_asistencias."Asistencia"  WHEN 1 THEN %s WHEN 0 '
+                                          'THEN %s END as Asistencia, asistencias_asistencias.fecha, '
+                                          'curso.nombre AS curso, materia.nombre AS materia, horario.docente_id '
+                                          'FROM asistencias_asistencias '
+                                          'INNER JOIN lista_listado ON '
+                                          '"public".asistencias_asistencias."Listado_id" = "public".lista_listado."id" '
+                                          'INNER JOIN alumno ON lista_listado.alumno_id = alumno."id" '
+                                          'INNER JOIN curso ON lista_listado.curso_id = curso."id" '
+                                          'INNER JOIN horario ON horario.curso_id = curso."id" '
+                                          'AND asistencias_asistencias."Horario_id" = horario."id" '
+                                          'INNER JOIN silabo ON horario.silabo_id = silabo."id" '
+                                          'INNER JOIN materia ON silabo.materia_id = materia."id" '
+                                          'WHERE materia.nombre=%s AND curso.nombre = %s '
+                                          'AND asistencias_asistencias.fecha = %s AND horario.docente_id = %s '
+                                          'GROUP BY alumno.nombres, alumno.apellidos, '
+                                          'asistencias_asistencias."Asistencia", '
+                                          'asistencias_asistencias.fecha,curso.nombre , '
+                                          'materia.nombre, horario.docente_id '
+                                          'order by alumno.apellidos asc', [p, f, materia, nomCurso, fecha, a])
+    contexto = {'listaalumno': listaalumno}
+
+    return render(request, "back-end/alumno/alumnoindex2.html", contexto)
+def asistencia_list_fecha(request):
+    a = request.user.id
+    materia = request.GET.get('materia')
+    nomCurso = request.GET.get('nomCurso')
+    listaasistencia = Asistencias.objects.raw('SELECT asistencias_asistencias.fecha as id, curso.nombre, '
+                                              'materia.nombre as materia,horario.docente_id  '
+                                              'FROM asistencias_asistencias '
+                                              'INNER JOIN lista_listado '
+                                              'ON asistencias_asistencias."Listado_id" = lista_listado."id" '
+                                              'INNER JOIN curso ON lista_listado.curso_id = curso."id" '
+                                              'INNER JOIN horario ON horario.curso_id = curso."id" '
+                                              'AND asistencias_asistencias."Horario_id" = horario."id" '
+                                              'INNER JOIN silabo ON horario.silabo_id = silabo."id" '
+                                              'INNER JOIN materia ON silabo.materia_id = materia."id" '
+                                              'WHERE materia.nombre = %s and curso.nombre= %s and '
+                                              'horario.docente_id =%s GROUP BY horario.docente_id,'
+                                              'asistencias_asistencias.fecha, "public".curso.nombre, '
+                                              'materia.nombre', [materia, nomCurso, a])
+    contexto = {'listaasistencia': listaasistencia}
+    return render(request, "back-end/asistencia/asistenciaindex_fechas.html", contexto)
+def vista_asistencias(request):
+    form = AsistenciasForm()
+    if request.method == 'POST':
+        form = AsistenciasForm(request.POST)
+    return render(request, 'back-end/asistencia/asistencias_form.html', {'form': form})
+
+def get_asistencias(request):
+    materiaId = request.POST["materia_id"]
+    periodoId = request.POST["periodo_id"]
+    cursoId = request.POST["curso_id"]
+    desde = request.POST["desde"]
+    hasta = request.POST["hasta"]
+    data = transpose(materiaId, periodoId, cursoId, desde, hasta)
+    if data:
+        df = pd.DataFrame(data)
+        df.fillna('0', inplace=True)
+        pivot = pd.pivot_table(df, index=['Alumno'], values=['Asistencia'], columns=['Fecha'], aggfunc='mean')
+        html2 = pivot.to_html(table_id='asistencia', classes='table table-striped table-bordered table-hover')
+        response = {"tablaHtml":html2}
+    else:
+        response = {"resp": 'False'}
+    return HttpResponse(json.dumps(response), content_type="application/json")
+
+
+
+def transpose(materiaId, periodoId, cursoId, desde, hasta):
+    asistencias = Asistencias.objects.filter(Horario__asignar__materia_id=materiaId, Listado__curso__id=cursoId,
+                                             Listado__periodo_id=periodoId, fecha__range=[desde, hasta])
+    data = [{'Alumno': a.Listado.alumno.apellidos+' '+a.Listado.alumno.nombres, 'Fecha': a.fecha,
+             'Asistencia': a.Asistencia} for a in asistencias]
+    return data
+
+
+def save_asistencia(request):
+    data = {}
+    fecha_hoy = date.today()
+    if request.method == 'POST':
+        datos = json.loads(request.POST['datos'])
+        for sub in datos:
+           if Asistencias.objects.filter(Horario_id=sub['horario'], Listado_id=sub['listado'], fecha=fecha_hoy):
+               data['resp'] = False
+           else:
+               h = Horario.objects.get(pk=sub['horario'])
+               h.asist_alum = 2
+               h.save()
+               n = Asistencias()
+               n.Horario_id = sub['horario']
+               n.Listado_id = sub['listado']
+               n.fecha = fecha_hoy
+               n.Asistencia = sub['asistencia']
+               n.save()
+               data['resp'] = True
+        return HttpResponse(json.dumps(data), content_type="application/json")
---
+++ apps/lista/views.py
@@ -0,0 +1,157 @@
+from django.shortcuts import render, redirect
+from .models import *
+from apps.lista.models import Listado
+from django.urls import reverse_lazy
+from django.views.generic import *
+from apps.lista.forms import ListadoForm
+from django.http import *
+from django.conf import settings
+from io import BytesIO
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4, landscape, letter
+from reportlab.lib.units import inch, mm, cm
+from reportlab.lib.fonts import *
+from django.views.generic import ListView
+from reportlab.lib import colors
+from reportlab.platypus import TableStyle, Table, Paragraph
+from datetime import date
+from reportlab.lib.styles import (ParagraphStyle, getSampleStyleSheet)
+
+from reportlab.lib.enums import TA_CENTER
+
+opc_icono = 'fa fa-address-card-o'
+opc_ruta = '/matriculas/'
+opc_nuevo = '/matriculas/nuevo'
+opc_crud = '/matriculas/crear/'
+opc_delete = '/matriculas/borrar'
+opc_entidad = 'Matriculas'
+
+
+def nuevo(request):
+    data = {
+        'icono': opc_icono, 'ruta': opc_ruta, 'crud': opc_crud, 'entidad': opc_entidad,
+        'boton': 'Guardar Matricula', 'action': 'add', 'titulo': 'Nuevo Registro de Matricula',
+    }
+    if request.method == 'GET':
+        data['form'] = ListadoForm()
+    return render(request, 'back-end/listado/listado_form.html', data)
+
+
+def crear(request):
+    f = ListadoForm(request.POST)
+    data = {
+        'icono': opc_icono, 'ruta': opc_ruta, 'crud': opc_crud
+    }
+    action = request.POST['action']
+    data['action'] = action
+    alumno = request.POST.get('alumno')
+    periodo = request.POST.get('periodo')
+    curso = request.POST.get('curso')
+    if request.method == 'POST' and 'action' in request.POST:
+        if Listado.objects.filter(alumno=alumno, curso=curso, periodo=periodo, estado=0):
+            f = ListadoForm(request.POST)
+            data = {
+                'icono': opc_icono, 'ruta': opc_ruta, 'crud': opc_crud, 'entidad': opc_entidad,
+                'boton': 'Guardar Matricula', 'action': 'add', 'titulo': 'Nuevo Registro de Matricula',
+                'form': f, 'dupla': 'Ya existe un registro igual'
+            }
+        else:
+
+            if f.is_valid():
+                f.save()
+                a = Alumno.objects.get(id=alumno)
+                a.estado = 0
+                a.save()
+            else:
+                data = {
+                    'icono': opc_icono, 'ruta': opc_ruta, 'crud': opc_crud, 'entidad': opc_entidad,
+                    'boton': 'Guardar Matricula', 'action': 'add', 'titulo': 'Nuevo Registro de Matricula',
+                    'form': f
+                }
+                return render(request, 'back-end/listado/listado_form.html', data)
+            return HttpResponseRedirect('/matriculas/lista')
+        return render(request, 'back-end/listado/listado_form.html', data)
+
+
+def reporte(request, id_lista):
+    response = HttpResponse(content_type='application/pdf')
+    buffer = BytesIO()
+    pdf = canvas.Canvas(buffer, pagesize=landscape(letter))
+    today = date.today()
+    format = today.strftime('%d/%m/%Y')
+    archivo_imagen = settings.MEDIA_ROOT + '/msp.jpg'
+    pdf.drawImage(archivo_imagen, 25, 500, 120, 90, preserveAspectRatio=True)
+    pdf.setFont("Helvetica-Bold", 30)
+    pdf.drawString(270, 550, 'Unidad Educativa')
+    pdf.setFont('Helvetica', 25)
+    pdf.drawString(280, 520, '"' 'Mis Primeros Pasos' '"')
+    pdf.setFont("Helvetica-Bold", 40)
+    pdf.drawString(80, 450, "COMPROBANTE DE MATRICULA")
+    pdf.setFont("Helvetica-Bold", 14)
+    pdf.drawString(10 * cm, 80, "Cpa. Yolanda Maldonado Bastidas")
+    pdf.setFont("Helvetica-Bold", 14)
+    pdf.drawString(13 * cm, 65, "Directora")
+    pdf.line(9 * cm, 100, 19 * cm, 100)
+    pdf.setFillColor("red")
+    pdf.setFont("Helvetica-Bold", 18)
+
+    pdf.drawString(690, 550, format)
+    y = 250
+    encabezados = ('Alumno', 'Cédula', 'Curso', 'Periodo', 'Representante ', 'Fecha')
+    # Creamos una lista de tuplas que van a contener a las personas
+    detalles = []
+    styles = getSampleStyleSheet()
+    yourStyle = ParagraphStyle('yourtitle',
+                               valing='MIDDLE',
+                               fontSize=16,
+                               alignment=0,
+                               leading=18,
+                               borderPadding=(2, 2, 2, 2)
+                               )
+    # [(matricula.alumno, matricula.alumno.cedula, matricula.curso, matricula.periodo,
+    #   matricula.fecha.strftime('%d/%m/%Y'))
+    for matricula in Listado.objects.filter(id=id_lista):
+        p = Paragraph(str(matricula.alumno), yourStyle)
+        r = Paragraph(str(matricula.alumno.representante), yourStyle)
+        detalles.append((p, matricula.alumno.cedula, matricula.curso.nombre, matricula.periodo, r,
+                         matricula.fecha.strftime('%d/%m/%Y')))
+    # lista_nombres = []
+    # for var in Listado.objects.filter(id=id_lista):
+    #   lista_nombres.append((var.alumno, var.curso, var.periodo))
+
+    # print(lista_nombres)
+    width, height = A4
+    # Establecemos el tamaño de cada una de las columnas de la tabla
+    detalle_orden = Table([encabezados] + detalles, colWidths=[5 * cm, 3.5 * cm, 3.5 * cm, 3.5 * cm, 5.5 * cm, 3 * cm],
+                          rowHeights=[1.5 * cm, 1.5 * cm])
+    # Aplicamos estilos a las celdas de la tabla
+    detalle_orden.setStyle(TableStyle(
+        [
+            ('GRID', (0, 0), (-1, -1), 0.5, colors.grey),
+            ('BOX', (0, 0), (-1, -1), 2, colors.black),
+            ('BACKGROUND', (0, 0), (-1, 0), colors.pink),
+            ('ALIGN', (0, 0), (5, 0), 'CENTER'),
+            ('ALIGN', (1, 1), (5, 1), 'CENTER'),
+            ('FONTSIZE', (0, 0), (5, 0), 18),
+            ('FONTSIZE', (0, 1), (5, 1), 16),
+            ('FONTNAME', (0, 0), (5, 0), 'Courier-Bold'),
+            ('VALIGN', (0, 0), (-1, -1), 'MIDDLE')
+        ]
+    ))
+    # Establecemos el tamaño de la hoja que ocupará la tabla
+    detalle_orden.wrapOn(pdf, width, height)
+    # Definimos la coordenada donde se dibujará la tabla
+    detalle_orden.drawOn(pdf, 75, y)
+    pdf.showPage()
+    pdf.save()
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+    return response
+
+
+def Matriculas_lista(request):
+    matriculas_list = Listado.objects.filter(estado=0)
+    contexto = {'matriculas_list': matriculas_list}
+    return render(request, "back-end/listado/listado.html", contexto)
+
