1c65fc3b80f96cbbcc974c039ec1a16455adf7c1
Chrisstianandres <Chrisstianandres@gmail.com>
2020-04-28 11:11:07 -0500

    Version 6

--- Demo/urls.py
+++ Demo/urls.py
@@ -79,7 +79,6 @@ urlpatterns = [
     # url(r'^alumno/', include('apps.asistencias.urls', namespace='alumno_list')),
 
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
